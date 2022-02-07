e514058cc498fa5835be86e2d7b34765760eb4c2
vanlgo <goellevan@gmail.com>
2022-02-07 05:01:42 +0000

    4 queries finished

--- 4_queries/2_assisstance_requests_data.sql
+++ 4_queries/2_assisstance_requests_data.sql
@@ -1,5 +1,6 @@
-SELECT teachers.name 
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
 FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
-WHERE name = 'Elliot Dickinson'
-GROUP BY students.name;
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
